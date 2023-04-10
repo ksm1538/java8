755fe919cc7ae77616831cdcb4d989aa132158e9
ksm15 <ksm1538@naver.com>
2023-04-11 00:48:13 +0900

    2

--- src/main/java/com/java8/ksm/Example2.java
+++ src/main/java/com/java8/ksm/Example2.java
@@ -1,11 +1,15 @@
 package com.java8.ksm;
 
 import java.lang.annotation.Annotation;
+import java.util.function.Function;
 
 public class Example2 {
-
 	public static void main(String[] args) {
+		Function<Integer, Integer> plus50 = (num) -> num+50;
+		System.out.println(plus50.apply(10));
 
+		Function<Integer, Integer> divide = (num) -> num/2;
+		System.out.println(divide.apply(10));
 	}
 
 }
